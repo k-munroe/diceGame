d2e7449db91735b49fdb97f17eebf0a63b361d1a
Kamanzi Rama-Munroe <kmunroe@mailfence.com>
2022-08-30 23:38:41 -0400

    include text to display who won or if tied

--- index.js
+++ index.js
@@ -18,3 +18,14 @@ image1.setAttribute("src", randomImageSource);
 
 image2.setAttribute("src", randomImageSourceTwo);
 
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 Wins";
+}
+
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+
+else{
+    document.querySelector("h1").innerHTML = "It's a tie!";
+}
